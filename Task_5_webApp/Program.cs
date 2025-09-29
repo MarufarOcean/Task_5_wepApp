62d4de6a54ad3176c72ccb20d43937c3a37828a9
MarufarOcean <marufarocean@gmail.com>
2025-09-29 11:21:18 +0600

    fixed image

--- Task_5_webApp/Program.cs
+++ Task_5_webApp/Program.cs
@@ -34,11 +34,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseStaticFiles();
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseStaticFiles();
 
 //Global guard for all requests except login/register/confirm
 app.Use(async (ctx, next) =>
